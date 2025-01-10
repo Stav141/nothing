14fd6ed848b0f1968bf6752e4c265a0e87f7c22a
sтav2 <ikcc2@mail.ru>
2025-01-10 20:10:48 +0100

    yum

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 
-// Disable context menu if necessary
+// Disable context menu
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 
@@ -9,5 +9,61 @@ window.addEventListener("load", vanish);
 
 function vanish() {
   loader.classList.add("disppear");
+
 }
 
+
+
+
+document.querySelector('#firstclick').addEventListener("click", () => {
+  loader.style.display = "none";
+
+  firstclick.style.display = "none";
+});
+
+
+
+
+/*
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.pageX;
+  const mouseY = event.pageY;
+
+  let closestButton = null;
+  let closestDistance = Infinity;
+
+  buttons.forEach((button) => {
+      const rect = button.getBoundingClientRect();
+      const buttonX = rect.left + rect.width / 2;
+      const buttonY = rect.top + rect.height / 2;
+
+      const distance = Math.sqrt(
+          Math.pow(mouseX - buttonX, 2) + Math.pow(mouseY - buttonY, 2)
+      );
+
+
+      if (distance < closestDistance) {
+          closestDistance = distance;
+          closestButton = button;
+      }
+  });
+
+
+  buttons.forEach((button) => {
+      button.classList.toggle("closest", button === closestButton);
+      
+  });
+
+
+  if (closestButton) {
+      closestButton.focus();
+  }
+});
+
+document.addEventListener("click", () => {
+  const focusedButton = document.querySelector(".buttonbox.closest");
+  if (focusedButton) {
+      focusedButton.click();
+  }
+});
+*/
