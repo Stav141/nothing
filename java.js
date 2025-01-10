1d8b1faed1171dbb2964db4eeab138d9120c9fa7
sтav2 <ikcc2@mail.ru>
2025-01-10 01:58:55 +0100

    did stuff
    modified the buttons(gave it a black background, changed the font to Pixel Operator), gave use to one of the buttons, added a loading screen (so the select.mp3 gets a little more time to load) and that's it i think

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
 
 // Disable context menu if necessary
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disppear");
+}
+
